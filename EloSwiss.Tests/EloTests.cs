6b0d2cb3493503f010c67d5b51e0dd5c30bd97a7
Scott Mackenzie <Scott.Mackenzie@myfoodbag.co.nz>
2020-01-22 09:17:12 +1300

    revised kfactor n HFA

--- EloSwiss.Tests/EloTests.cs
+++ EloSwiss.Tests/EloTests.cs
@@ -40,7 +40,16 @@ namespace EloSwiss.Tests
             var resultsHome = Elo.Score(1000, 1000, Winner.Player1, Played.Home);
             var results = Elo.Score(1000, 1000, Winner.Player1);
             results.ratingA.Should().BeGreaterThan(results.ratingB);
-            results.ratingA.Should().BeGreaterThan(resultsHome.ratingA);
+            resultsHome.ratingA.Should().BeGreaterThan(results.ratingA);
+        }
+
+        [Fact]
+        public void Ratings_ScoresMatch_WithHomeAdvantage_RevisedHFA()
+        {
+            var resultsHome = Elo.Score(1500, 1500, Winner.Player1, Played.Home, kFactor: 20);
+            var results = Elo.Score(1500, 1500, Winner.Player1, 20);
+            results.ratingA.Should().BeGreaterThan(results.ratingB);
+            resultsHome.ratingA.Should().BeGreaterThan(results.ratingA);
         }
     }
 }
--- EloSwiss/Elo.cs
+++ EloSwiss/Elo.cs
@@ -12,9 +12,13 @@ namespace EloSwiss
         Home, Away
     }
 
+    /// <summary>
+    /// relationship between HFA & kFactor
+    /// http://opisthokonta.net/?p=1387
+    /// </summary>
     public static class Elo
     {
-        private static double HOME_ADVANTAGE = 100.0d;
+        private static double HOME_ADVANTAGE = 68.75d;
         public static (double expectedA, double expectedB) Probability(double ratingA, double ratingB)
         {
             var expectedScoreA = 1 / (1 + Math.Pow(10, (ratingB - ratingA) / 400));
@@ -22,7 +26,7 @@ namespace EloSwiss
             return (expectedA: expectedScoreA, expectedB: expectedScoreB);
         }
 
-        public static (double ratingA, double ratingB) Score(double ratingA, double ratingB, double scoreA, double scoreB, int kFactor=30)
+        public static (double ratingA, double ratingB) Score(double ratingA, double ratingB, double scoreA, double scoreB, int kFactor=20)
         {
             var (expectedA, expectedB) = Probability(ratingA, ratingB);
             var newRatingA = ratingA + (kFactor * (scoreA - expectedA));
@@ -30,10 +34,10 @@ namespace EloSwiss
             return (newRatingA, newRatingB);
         }
 
-        public static (double ratingA, double ratingB) Score(double ratingA, double ratingB, Winner winner, int kFactor = 30) 
+        public static (double ratingA, double ratingB) Score(double ratingA, double ratingB, Winner winner, int kFactor = 20) 
             => Score(ratingA, ratingB, winner == Winner.Player1 ? 1 : 0, winner == Winner.Player2 ? 1 : 0, kFactor: kFactor);
 
-        public static (double ratingA, double ratingB) Score(double ratingA, double ratingB, Winner winner, Played playedPlayerA, int kFactor = 30)
+        public static (double ratingA, double ratingB) Score(double ratingA, double ratingB, Winner winner, Played playedPlayerA, int kFactor = 20)
             => Score(playedPlayerA == Played.Home ? ratingA + HOME_ADVANTAGE : ratingA, 
                 playedPlayerA == Played.Away ? ratingB + HOME_ADVANTAGE : ratingB, 
                 winner == Winner.Player1 ? 1 : 0, 
